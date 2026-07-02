ca780e6a1b72672ecadcc7ae6b99889499ac2be4
Ivan <kotov@zov.monster>
2026-07-02 23:09:58 +0300

    improve timestamp handling

--- response.go
+++ response.go
@@ -1,6 +1,10 @@
 package sova
 
-import "time"
+import (
+	"encoding/json"
+	"errors"
+	"time"
+)
 
 var _ error = (*ResponseError)(nil)
 
@@ -14,12 +18,6 @@ func (e ResponseError) Error() string {
 }
 
 // player/{id}
-
-type Timestamp string
-
-func (t Timestamp) Parse() (time.Time, error) {
-	return time.Parse(time.RFC3339Nano, string(t))
-}
 
 type Duration int
 
@@ -44,19 +42,104 @@ type Statistics struct {
 	PlayTime   Duration `json:"play_time"`
 }
 
+const TimestampFormat = time.RFC3339Nano
+
+type Timestamp string
+
+func (t Timestamp) Parse() (time.Time, error) {
+	return time.Parse(TimestampFormat, string(t))
+}
+
 type Punishment struct {
 	Reason    string     `json:"reason"`
+	ExpiresAt *time.Time `json:"expires_at,omitempty"`
+}
+
+type punishmentJSON struct {
+	Reason    string     `json:"reason"`
 	ExpiresAt *Timestamp `json:"expires_at,omitempty"`
+}
+
+var ErrInvalidServerTimestamp = errors.New("invalid server timestamp format")
+
+func (pu *Punishment) MarshalJSON() ([]byte, error) {
+	var t *Timestamp
+	if pu.ExpiresAt != nil {
+		t = (*Timestamp)(new(pu.ExpiresAt.Format(TimestampFormat)))
+	}
+	return json.Marshal(punishmentJSON{
+		Reason:    pu.Reason,
+		ExpiresAt: t,
+	})
+}
+
+func (pu *Punishment) UnmarshalJSON(data []byte) error {
+	var temp punishmentJSON
+	if err := json.Unmarshal(data, &temp); err != nil {
+		return err
+	}
+	if pu.Reason = temp.Reason; temp.ExpiresAt == nil {
+		return nil
+	}
+	t, err := temp.ExpiresAt.Parse()
+	if err != nil {
+		return ErrInvalidServerTimestamp
+	}
+	pu.ExpiresAt = &t
+	return nil
 }
 
 type Player struct {
 	ID         int         `json:"id"`
 	Nickname   string      `json:"nickname"`
-	CreatedAt  Timestamp   `json:"created_at"` //TODO: джавид гондон
+	CreatedAt  time.Time   `json:"created_at"`
 	Rank       Rank        `json:"rank"`
 	Statistics Statistics  `json:"statistics"`
 	Ban        *Punishment `json:"ban,omitempty"`
 	Mute       *Punishment `json:"mute,omitempty"`
+}
+
+type playerJSON struct {
+	ID         int         `json:"id"`
+	Nickname   string      `json:"nickname"`
+	CreatedAt  Timestamp   `json:"created_at"`
+	Rank       Rank        `json:"rank"`
+	Statistics Statistics  `json:"statistics"`
+	Ban        *Punishment `json:"ban,omitempty"`
+	Mute       *Punishment `json:"mute,omitempty"`
+}
+
+func (p *Player) MarshalJSON() ([]byte, error) {
+	return json.Marshal(playerJSON{
+		ID:         p.ID,
+		Nickname:   p.Nickname,
+		CreatedAt:  Timestamp(p.CreatedAt.Format(TimestampFormat)),
+		Rank:       p.Rank,
+		Statistics: p.Statistics,
+		Ban:        p.Ban,
+		Mute:       p.Mute,
+	})
+}
+
+func (p *Player) UnmarshalJSON(data []byte) error {
+	var temp playerJSON
+	if err := json.Unmarshal(data, &temp); err != nil {
+		return err
+	}
+	t, err := temp.CreatedAt.Parse()
+	if err != nil {
+		return ErrInvalidServerTimestamp
+	}
+	*p = Player{
+		ID:         temp.ID,
+		Nickname:   temp.Nickname,
+		CreatedAt:  t,
+		Rank:       temp.Rank,
+		Statistics: temp.Statistics,
+		Ban:        temp.Ban,
+		Mute:       temp.Mute,
+	}
+	return nil
 }
 
 type PlayerResponse = Player
